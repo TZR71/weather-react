01b27d94eb5e6b4c01d7e3172738eab050bf5334
TZR71 <Tiinarojola@gmail.com>
2024-11-15 14:36:59 +0100

    Installed bootstrap

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>
-
-      Weather App</h1>
-      <footer>
-        This project was created by Tiina Rojola and is open-sourced on {""} 
-        <a
-          href="https://github.com/TZR71/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >          
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Tiina Rojola and is open-sourced on {""}
+          <a
+            href="https://github.com/TZR71/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
